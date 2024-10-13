8c8020203f6027c27a3e8fad227e271630ef273e
AntDBuck <antonybuck@outlook.com>
2024-10-13 16:46:33 +0100

    Delete Week1 directory

--- Week1/index.js
+++
@@ -1,9 +0,0 @@
-console.log("This is my first programme");
-console.log("Welcome John, your month's salary is £50,000");
-
-const num1 = 5;
-const num2 = 3;
-// Add two numbers
-const sum = num1 + num2;
-// Display the sum
-console.log("The sum of " + num1 + " and " + num2 + " is " + sum);
